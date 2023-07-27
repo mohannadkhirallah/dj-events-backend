3e4874cb7dfd188e15fbe4a126a058e6e405ad1b
Mohannad Kheralla <mkheralla@Netways.com>
2023-07-27 12:26:35 +0300

    setup prod database file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,17 @@
-/* ./config/env/production/server.js */
+// Path: ./config/env/production/server.js
+// starting from Strapi v 4.6.1 server.js has to be the following
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'app-strapi-portal-dev.azurewebsites.net',
-});
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env('MY_HEROKU_URL'),
+  app: {
+    keys: env.array('APP_KEYS')
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+})
